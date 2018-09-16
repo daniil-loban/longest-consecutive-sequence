bc411d76cdc1878401b795bd403320a1d4507123
Daniil Loban <dungeon_lda@mail.ru>
2018-09-16 16:22:39 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+    let searchBase     = new Set()
+    let maxChainLength = 0
+
+    for (let i=0; i< array.length; ++i){
+        searchBase.add(array[i])
+    }
+
+    let startValue;
+    for (let i=0; i<array.length; ++i) {
+        startValue =array[i]; // Test the value
+
+        // If chain for it is not exist         
+        if (!searchBase.has(startValue-1)){  
+            
+            endValue = startValue;  // Open a new chain 
+            while(searchBase.has(endValue)){
+                endValue++;         // Increase length of chain
+            }
+            // Compare an select max result 
+            maxChainLength = (maxChainLength > endValue-startValue)? 
+                               maxChainLength
+                             : endValue-startValue;
+        }   
+    } 
+           
+    return maxChainLength;
 }
+
