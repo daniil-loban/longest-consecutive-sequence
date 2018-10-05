7ba5e5d349647cd282801ad7278db9080069a782
daniil-loban <dungeon_lda@mail.ru>
2018-10-05 22:54:50 +0300

    the speed optimization by a Set

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+    if (array.length == 0)
+        return 0
+
+    let searchBase = new Set(array),
+        maxChainLength = 1,
+        startValue = 0,
+        startBase = new Set();
+
+    for (let key of searchBase) {
+        if (!searchBase.has(key-1) && searchBase.has(key+1)){
+            startBase.add(key)
+        }
+    }
+
+    if (startBase.length == 0){
+        return 1
+    }
+
+    for (let key of startBase) {
+        startValue = key;                 // Test the value
+        endValue   = startValue;          // Open a new chain 
+        while(searchBase.has(endValue)){
+            endValue++;                   // Increase length of chain
+        }
+        maxChainLength = (maxChainLength > endValue-startValue)
+                       ? maxChainLength
+                       : endValue-startValue;
+    } 
+    
+    return maxChainLength;
 }
+
